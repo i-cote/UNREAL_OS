c2be86aa5a4c14dc320fd392fba041efbce5aaa8
Your Name <Irvin Cote>
2022-10-12 22:14:48 -0300

    ...

---
+++ Kernel/include/interrupts.h
@@ -0,0 +1,21 @@
+ /*
+ *   interrupts.h
+ *
+ *  Created on: Apr 18, 2010
+ *      Author: anizzomc
+ */
+
+#ifndef INTERRUPS_H_
+#define INTERRUPS_H_
+
+#include <idtLoader.h>
+
+void _irq01Handler(void);
+
+void _cli(void);
+
+void _sti(void);
+
+void picMasterMask(uint8_t mask);
+
+#endif /* INTERRUPS_H_ */
