1f26d426ac481a662dd2be166865921432ceb2fa
Your Name <Irvin Cote>
2022-10-14 10:43:34 -0300

    ...

---
+++ Kernel/include/keyboard_driver_asm.h
@@ -0,0 +1,6 @@
+#ifndef KEYBOARD_DRIVER_ASM_H
+#define KEYBOARD_DRIVER_ASM_H
+#include <stdint.h>
+extern uint8_t fetchScanCodeByte();
+extern void setScanCodeSetToOne();
+#endif /* KEYBOARD_DRIVER_ASM_H */
