ea5f4807aefde6a494bb27f2844eb1ea22ccf137
Your Name <Irvin Cote>
2022-10-18 18:20:14 -0300

    ...

--- docker_build_command.sh
+++ docker_build_command.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 killall qemu-system-x86_64
 make clean
+make -C ./Toolchain clean
+docker run --rm -v ${PWD}:/root --security-opt seccomp:unconfined agodio/itba-so:1.0 make -C /root/Toolchain
 docker run --rm -v ${PWD}:/root --security-opt seccomp:unconfined agodio/itba-so:1.0 make -C /root
 if [[ !  $? -eq 0 ]]
 	then 
