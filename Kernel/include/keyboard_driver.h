2209a67d08470b076a3f6bf52a53d1d8e44055fd
Your Name <Irvin Cote>
2022-10-18 15:53:47 -0300

    ...

--- Kernel/include/keyboard_driver.h
+++ Kernel/include/keyboard_driver.h
@@ -1,5 +1,11 @@
 #ifndef KEYBOARD_DRIVER_H_
 #define KEYBOARD_DRIVER_H_
+#include <stdint.h>
 void fetchKeyboardEvent();
 void initialiseKeyboard();
+struct pressedKeys
+{
+	uint16_t keys[4];
+	uint8_t nbOfPressedKeys;
+};
 #endif /*KEYBOARD_DRIVER_H_*/
