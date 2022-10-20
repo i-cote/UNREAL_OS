2b2008806b0e9beb22db3794bd846faad273b731
Your Name <Irvin Cote>
2022-10-20 16:40:03 -0300

    ...

--- Kernel/argentinian.consolemap.h
+++ Kernel/argentinian.consolemap.h
@@ -2,52 +2,50 @@ static struct language_mapping ar_mapping =
 	{
 		.main_map = {
 			"\x1b"/*escape*/,
-			"&","é",
-			"\x22"/*double quote*/,
-			"\x27"/*single quote*/,
-			"(","-","è","_","ç","à",")","=",
+			"1","2","3","4","5","6","7","8","9","0",
+			UNDEF,UNDEF,
 			"\x7f"/*backspace*/,
 			"\x09"/*tab*/,
-			"a","z","e","r","t","y","u","i","o","p","^","$",
+			"q","w","e","r","t","y","u","i","o","p",
+			UNDEF,UNDEF,
 			"\x0d"/*enter*/,
 			STATE_CHANGE_KEY,/*crtl*/
-			"q","s","d","f","g","h","j","k","l","m","ù",
-			(char *)0,/*² not implemented by our console*/
+			"a","s","d","f","g","h","j","k","l",
+			UNDEF,UNDEF,UNDEF,
 			STATE_CHANGE_KEY,/*shift left*/
-			"*","w","x","c","v","b","n",",",";",":","!",
+			UNDEF,
+			"z","x","c","v","b","n","m",",",".","-",
 			STATE_CHANGE_KEY,/*shift right*/
 			UNDEF,
 			STATE_CHANGE_KEY,/*alt*/
 			" ",
 			STATE_CHANGE_KEY,/*caps lock*/
 	
-			UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,
-	
-			"<"
+			UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF
 		},
 	
 		.shift_map = {
 			"\x1b"/*escape*/,
-			"1","2","3","4","5","6","7","8","9","0","°","+",
+			"1","2","3","4","5","6","7","8","9","0",
+			UNDEF,UNDEF,
 			"\x7f"/*backspace*/,
 			"\x09"/*tab*/,
-			"A","Z","E","R","T","Y","U","I","O","P",
-			UNDEF,/*¨ not implemented by our console*/
-			"£",
+			"Q","W","E","R","T","Y","U","I","O","P",
+			UNDEF,UNDEF,
 			"\x0d"/*enter*/,
 			STATE_CHANGE_KEY,/*crtl*/
-			"Q","S","D","F","G","H","J","K","L","M","%","~",
+			"A","S","D","F","G","H","J","K","L",
+			UNDEF,UNDEF,UNDEF,
 			STATE_CHANGE_KEY,/*shift left*/
-			"µ","W","X","C","V","B","N","?",".","/","§",
+			UNDEF,
+			"Z","X","C","V","B","N","M",";",":","_",
 			STATE_CHANGE_KEY,/*shift right*/
 			UNDEF,
 			STATE_CHANGE_KEY,/*alt*/
 			" ",
 			STATE_CHANGE_KEY,/*caps lock*/
 	
-			UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,
-	
-			">"
+			UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF,UNDEF
 		},
 
 		.specialKeys = {
