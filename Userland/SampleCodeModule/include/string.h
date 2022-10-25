6b9f16ba7831e534e46f62b45895f173c7cf5404
TheJohnFaraday <jfernandezdinardo@itba.edu.ar>
2022-10-24 22:01:38 -0300

    parser works (strtok)

--- Userland/SampleCodeModule/include/string.h
+++ Userland/SampleCodeModule/include/string.h
@@ -9,5 +9,7 @@ int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, uint32_t n);
 int strlen(const char *s);
 int strtol(const char *str, char **endptr, int base);
+char * my_strtok(char *srcString, char *delim);
+unsigned int is_delim(char c, char *delim);
 
 #endif
--- Userland/SampleCodeModule/shell.c
+++ Userland/SampleCodeModule/shell.c
@@ -14,10 +14,9 @@ de memoria posteriores a la misma.
 #include <tron.h>
 
 
-
 void shell(){
     printShellMenu();
-    tron();
+   // tron();
     waiting_command();
 }
 
@@ -39,25 +38,28 @@ void waiting_command(){
         }
         
         //Preparing the command for read
-        //printf("no se rompe");
-        //printf("%d",count);
-        char toRead[count+1];
+        //First i get the line 
+        char line[count+1];
         for (int i = 0; i < count; i++)
         {
-            toRead[i] = command_reader[i];
+            line[i] = command_reader[i];
+            
         }
-        toRead[count]=0;
+        line[count]='\0';
+        //Now i use strtok for delete the spaces
+        char * toRead = my_strtok(line, SPACE);
         
-        reading_command(count, toRead);
+        
+        reading_command(toRead);
     }
 }
 
-void reading_command(int size, char * str){
+void reading_command(char * str){
 
     int found = 0;
     int to_execute;
     for (int i = 0; i < COMMAND_NUMBER && !found; i++){
-        if (0==strcmp(commands[i].name, str)){
+        if (!(strcmp(commands[i].name, str))){
             found = 1;
             to_execute = i;
         }
